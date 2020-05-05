994074c290da39f74095fae18c3b2ef6fd59bf49
Igor <workuglis@gmail.com>
2020-05-05 16:03:36 +0300

    * 1. Создать модель User [#279231]

---
+++ src/main/java/ru/job4j/map/User.java
@@ -0,0 +1,12 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+
+/**
+ * 1. Создать модель User [#279231]
+ */
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+}
