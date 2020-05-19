220e5493241df66f30a64455b922765daf65e8f5
Igor <workuglis@gmail.com>
2020-05-19 16:25:26 +0300

    Изменил экранирование путей к файлам

--- src/test/java/ru/job4j/io/SearchTest.java
+++ src/test/java/ru/job4j/io/SearchTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.core.Is.is;
@@ -23,11 +24,13 @@ public class SearchTest {
     @Test
     public void whenSearchEndWithTxt() {
         Path source = Paths.get("./src/data");
-        List<Path> expected = List.of(
-                Path.of("./src/data/server.txt"),
-                Path.of("./src/data/server1.txt"),
-                Path.of("./src/data/target.txt"),
-                Path.of("./src/data/target1.txt")
+        List<Path> expected = new ArrayList<>(
+                List.of(
+                        Path.of("./src/data/server.txt"),
+                        Path.of("./src/data/server1.txt"),
+                        Path.of("./src/data/target.txt"),
+                        Path.of("./src/data/target1.txt")
+                )
         );
         List<Path> rsl = Search.search(source, "txt");
         assertThat(rsl, is(expected));
