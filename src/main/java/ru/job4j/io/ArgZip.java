6206fb9b5aa5a054582a6aeabea8a6ee432456c8
Igor <workuglis@gmail.com>
2020-05-21 14:32:05 +0300

    5.1. Архивировать проект. [#279261]

--- src/main/java/ru/job4j/io/ArgZip.java
+++ src/main/java/ru/job4j/io/ArgZip.java
@@ -4,25 +4,37 @@ package ru.job4j.io;
  * 5.1. Архивировать проект. [#279261]
  */
 public class ArgZip {
-//    private final String[] args;
-//
-//    public ArgZip(String[] args) {
-//        this.args = args;
-//    }
-//
-//    public boolean valid() {
-//        return args.length == 3;
-//    }
-//
-//    public String directory() {
-//        return args[0];
-//    }
-//
-//    public String exclude() {
-//        return args[1];
-//    }
-//
-//    public String output() {
-//        return args[2];
-//    }
+    private final String[] args;
+
+    public ArgZip(String[] args) {
+        this.args = args;
+    }
+
+    public boolean valid() {
+        if (args.length < 6) {
+            throw new IllegalArgumentException("Не правильные аргументы для запуска програмы");
+        }
+        return true;
+    }
+
+    public String directory() {
+        if (valid() && args[0].equals("-d")) {
+            return args[1];
+        }
+        return "";
+    }
+
+    public String exclude() {
+        if (valid() && args[2].equals("-e")) {
+            return args[3];
+        }
+        return "";
+    }
+
+    public String output() {
+        if (valid() && args[4].equals("-o")) {
+            return args[5];
+        }
+        return "";
+    }
 }
--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -1,7 +1,69 @@
 package ru.job4j.io;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
 /**
  * 5.1. Архивировать проект. [#279261]
  */
 public class Zip {
+    /**
+     * Упаковывыем в архив указаные файлы в списке.
+     *
+     * @param sources список файлов.
+     * @param target  путь и название собранного архив
+     */
+    public void packFiles(List<Path> sources, Path target) {
+        try (var zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target.toString())))) {
+            for (Path file : sources) {
+                ZipEntry zipEntry = new ZipEntry(file.toFile().getAbsolutePath().substring(3));
+                zip.putNextEntry(zipEntry);
+                try (var out = new BufferedInputStream(new FileInputStream(file.toFile().toString()))) {
+                    zip.write(out.readAllBytes());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Архивировать один файл.
+     *
+     * @param source нужынй файл.
+     * @param target путь и название к архиву.
+     */
+    public void packSingleFile(Path source, Path target) {
+        try (var zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target.toString())))) {
+            zip.putNextEntry(new ZipEntry(source.toFile().getAbsolutePath().substring(3)));
+            try (var out = new BufferedInputStream(new FileInputStream(source.toString()))) {
+                zip.write(out.readAllBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Создаём архив исходя из аргументов.
+     *
+     * @param args аргументы :
+     *             -d : указываем ныжную директорию.
+     *             -e : исключаем не нужные файлы.
+     *             -o : название и путь к полученому архиву.
+     * @throws IOException можем получить ошибку при вводе неверных данных.
+     */
+    public static void main(String[] args) throws IOException {
+        Zip zip = new Zip();
+        ArgZip argZip = new ArgZip(args);
+        SearchFiles searchFiles = new SearchFiles(path -> !(path.toFile().getName().endsWith(argZip.exclude())));
+        Files.walkFileTree(Paths.get(argZip.directory()), searchFiles);
+        List<Path> paths = searchFiles.getPaths();
+        zip.packFiles(paths, Paths.get(argZip.output()));
+    }
 }
